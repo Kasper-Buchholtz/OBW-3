ecf9c66df2e99fa52912e72d05feb3f7871cc871
Kasper Buchholtz <117350672+Kasper-Buchholtz@users.noreply.github.com>
2025-06-16 00:24:18 +0200

    test

--- apps/i18n/src/app/layout.tsx
+++ apps/i18n/src/app/layout.tsx
@@ -8,8 +8,7 @@ export default async function RootLayout({
   return (
     <html>
       <body className='selection:bg-lights-0 selection:text-darks-900 bg-darks-900'>
-        {process.env.NODE_ENV === 'production' ? null : children}
-        <WaitingPage />
+        {process.env.NODE_ENV === 'production' ? <WaitingPage /> : children}
       </body>
     </html>
   )
